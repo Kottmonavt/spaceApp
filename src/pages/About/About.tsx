27b9855b4455232544849f7d7fb7af08953ba1c9
Kotmonavt <kristina126zz@gmail.com>
2024-10-29 23:46:14 +0300

    api

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,13 +1,31 @@
-import './App.css'
+import { useState, useEffect } from 'react';
+import './About.css'
 
+const apiUrl = 'https://api.nasa.gov/planetary/apod?api_key=Zx2ZZcY4NVb8hhZzt0llkzXGdHaKCugh9NdJ87rk';
 
 function About() {
+    const [imageUrl, setImageUrl] = useState('');
 
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(apiUrl);
+          const data = await response.json();
+          setImageUrl(data.url); // Assuming the API returns an object with a 'imageUrl' property
+        } catch (error) {
+          console.error('Error fetching image URL:', error);
+        }
+      };
+  
+      fetchData();
+    }, []);
+  
     return (
-        <div>
-            
-           
+      <div>
+        <div className='box-for-images'>
+            {imageUrl && <img src={imageUrl} alt="Image from API" className='image-nasa' />}
         </div>
+      </div>
     );
-}
+  }
 export default About;
