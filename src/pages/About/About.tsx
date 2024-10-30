494d36dccc2601b8f6db7e44ac3c4b19394dc61a
Kotmonavt <kristina126zz@gmail.com>
2024-10-30 11:58:26 +0300

    add about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -24,6 +24,18 @@ function About() {
       <div>
         <div className='box-for-images'>
             {imageUrl && <img src={imageUrl} alt="Image from API" className='image-nasa' />}
+            <div className='text-image russo-24'>Astronomy Picture of the Day</div>
+        </div>
+        <div className='text-container-about'>
+          <div className='russo-48'>About our program</div>
+          <div className='roboto-28'>Hi friend!</div>
+          <div className='roboto-28'>
+            We are a young project to create space travel to different planets. 
+            With us, you can fulfill your childhood dream or just spend a lot of money. 
+            An exciting journey to the planets of the Solar system and the immense beauty of space are waiting for you. 
+            Just choose a travel plan and a planet, and we and our professional team will do the rest!
+          </div>
+          <div className='text-image roboto-28'>And every day on this page you can view the astronomical photo of the day from Nasa</div>
         </div>
       </div>
     );
