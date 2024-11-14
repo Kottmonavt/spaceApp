a180071a7b7ace254b420772f8acf4777782c907
Kotmonavt <kristina126zz@gmail.com>
2024-11-14 17:37:28 +0300

    add hover and new planet

--- src/pages/Planet/Planet.tsx
+++ src/pages/Planet/Planet.tsx
@@ -5,12 +5,11 @@ interface PlanetProps {
     describe: string;
     classImage: string;
     image: string;
-    cardClass: string;
 }
 
-export const PlanetCard = ({namePlanet, describe, classImage, image, cardClass}: PlanetProps) => {
+export const PlanetCard1 = ({namePlanet, describe, classImage, image}: PlanetProps) => {
     return (
-        <div className={cardClass}>
+        <div className='planet-inner-box'>
             <img src={image} alt="Img" className={classImage}/>
             <div className='text-planet'>
                 <div className='text-name-planet russo-24'>{namePlanet}</div>
@@ -20,27 +19,36 @@ export const PlanetCard = ({namePlanet, describe, classImage, image, cardClass}:
     )
 }
 
+export const PlanetCard2 = ({namePlanet, describe, classImage, image}: PlanetProps) => {
+    return (
+        <div className='planet-inner-box'>
+            <div></div>
+            
+            <div className='text-uranus'>
+                <div className='text-name-planet russo-24'>{namePlanet}</div>
+                <div className='roboto-18'>{describe}</div>
+            </div>
+            <img src={image} alt="Img" className={classImage}/>
+          
+        </div>
+    )
+}
+
+
 function Planet() {
     return (
-        <div>
+        <body>
             <div className='body-text'>
                 <div className='russo-36'>You can choose one of these planets for your journey</div>
             </div>
-            <div className='planet-container'>
-                <div className='row-card'>
-                    <div className='planet-card'>
-                        <PlanetCard namePlanet='01 MARS' describe='A small planet for the realization of all your childhood dreams of space travel.' classImage='image-mars' image='./src/assets/Mars.png' cardClass='planet-inner-box'/>
-                        <PlanetCard namePlanet='03 SATURN' describe='Have you ever dreamed of jumping on the rings of Saturn? It is possible with us' classImage='image-saturn' image='./src/assets/Saturn.png' cardClass='planet-inner-box'/>       
-                    </div>
-                    <div className='content1'>
-                        <PlanetCard namePlanet='02 URANUS' describe='Did you dream of becoming a polar explorer as a child? On Uranus, you can try yourself as a space polar explorer!' classImage='image-uranus' image='./src/assets/Uranus.png' cardClass='planet-card'/>
-                    
-                    </div>
-                    
-                </div>
-                 
+            <div className='planet-container'>                
+                <PlanetCard1 namePlanet='01 MARS' describe='A small planet for the realization of all your childhood dreams of space travel.' classImage='image-mars' image='./src/assets/Mars.png'/>
+                <PlanetCard2 namePlanet='02 URANUS' describe='Did you dream of becoming a polar explorer as a child? On Uranus, you can try yourself as a space polar explorer!' classImage='image-uranus' image='./src/assets/Uranus.png'/>
+                <PlanetCard1 namePlanet='03 MERCURY' describe='Do you like the sun and the heat? Then it is time for you to visit Mercury! The sun is closer and hotter here than you can imagine.' classImage='image-mercury' image='./src/assets/Mercury.png'/> 
+                <PlanetCard2 namePlanet='04 JUPITER' describe='Have you ever dreamed of watching a hurricane on Jupiter up close? Your dream can be realized with the help of us!' classImage='image-jupiter' image='./src/assets/Jupiter.png'/> 
+                <PlanetCard1 namePlanet='05 VENUS' describe='Do you like sauna and acid rain? Do you want Monday to come less often? We are waiting for you on Venus, here your dreams will become reality!' classImage='image-venus' image='./src/assets/Venus.png'/>                 
             </div>
-        </div>
+        </body>
     )
 }
 
--- src/pages/Team/Team.tsx
+++ src/pages/Team/Team.tsx
@@ -4,13 +4,15 @@ interface TeamCardProps {
     namePilot: string;
     role: string;
     image: string;
+    description: string;
 }
 
-export const TeamCard = ({namePilot, role, image}: TeamCardProps) => {
+export const TeamCard = ({namePilot, role, image, description}: TeamCardProps) => {
     return (
         
         <div className='image-card'>
             <img src={image} alt="Img" className='image'/>  
+            <div className='image overlay roboto-24'>{description}</div>
             <div className='pilot-name russo-24'>{namePilot}</div>
             <div className='pilot-role russo-24'>{role}</div>           
         </div>
@@ -22,9 +24,10 @@ function Team() {
         <div>
             <div className='title-container-team russo-36'>Our Team</div>
             <div className='image-box'>
-                <TeamCard namePilot='Shkaplerov A. N.' role='Spacecraft commander' image='./src/assets/Shkaplerov.jpg' />
-                <TeamCard namePilot='Artemyev O. G.' role='The first pilot' image='./src/assets/Artemyev.jpg' />
-                <TeamCard namePilot='Kononenko O. D.' role='The second pilot' image='./src/assets/Kononenko.jpg' />
+                <TeamCard namePilot='Shkaplerov A. N.' role='Spacecraft commander' image='./src/assets/Shkaplerov.jpg' description='Anton Nikolaevich Shkaplerov (born February 20, 1972, Sevastopol) is a Russian test cosmonaut of the Gagarin Research Institute of the Central Research Institute of the Russian Federation, Candidate of Technical Sciences, Colonel of the Russian Air Force (12/30/2009). He made four space flights to the International Space Station, during which he conducted three spacewalks — a total duration of 21 hours and 39 minutes. Hero of the Russian Federation (2013).'/>
+                <TeamCard namePilot='Artemyev O. G.' role='The first pilot' image='./src/assets/Artemyev.jpg' description='Oleg Germanovich Artemyev (born December 28, 1970, Riga, Latvian SSR) is a Russian test cosmonaut of the Gagarin Research Institute of the Central Research Institute of the USSR, the 118th cosmonaut of the USSR/Russia and the 534th cosmonaut in the world. The total duration of the flights was 560 days, 18 hours and 06 minutes. He made eight spacewalks, the total duration of work in outer space was more than 53 hours.'/>
+                <TeamCard namePilot='Kononenko O. D.' role='The second pilot' image='./src/assets/Kononenko.jpg' description='Oleg Dmitrievich Kononenko (born June 21, 1964, Chardzhou, Turkmen SSR) is a Russian cosmonaut, Hero of the Russian Federation, Hero of Turkmenistan.
+Instructor-test cosmonaut of the 1st class, 473rd cosmonaut of the world, 102nd cosmonaut of the Russian Federation.  The total time in space was 1,110 days, 14 hours and 57 minutes, including 44 hours and 30 minutes in outer space'/>
             </div>
         </div>
 
